340b9115ff4d9f3375da37266dba0574018483e8
Andrea <andreamongiellocasuale@gmail.com>
2024-03-28 14:44:51 +0100

    inizializzazione della classe dei suoi dati

---
+++ Cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self,nome,cognome,email,storico_appuntamenti):
+        self.nome=nome
+        self.cognome=cognome
+        self.email=email
+        self.storico_appuntamenti=storico_appuntamenti
