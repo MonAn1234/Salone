63f06b07ff365d0151dd3d9cbf54c9eaebc1e83c
Andrea <andreamongiellocasuale@gmail.com>
2024-03-28 14:44:24 +0100

    inizializzazione dell'appuntamento con i suoi dati

---
+++ Appuntamento.py
@@ -0,0 +1,7 @@
+class Appuntamento:
+    def __init__(self,appuntamento,data,ora,tipo_di_servizio,cliente):
+        self.appuntamento=appuntamento
+        self.data=data
+        self.ora=ora
+        self.tipo_di_servizio=tipo_di_servizio
+        self.cliente=cliente
